f3ba84ab239949398e0a1e7ff75c91cfb957b921
coolmsf <coolder.hsl@outlook.com>
2020-11-03 23:09:48 +0800

    simple backdoor

---
+++ Programming/C_Cpp/Network/backdoor/cmake-build-debug/CMakeFiles/bin_sh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bin_sh.dir/main.c.o"
+  "bin_sh"
+  "bin_sh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bin_sh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
