d7a21686285a8e0d8dab627e5bc76a0c467407cd
coolmsf <coolder.hsl@outlook.com>
2021-03-05 12:50:01 +0800

    day2

--- System/Linux/Kernel/30day_make_OS_coolder/day2/coolderOS.s
+++ System/Linux/Kernel/30day_make_OS_coolder/day2/coolderOS.s
@@ -2,66 +2,112 @@
 .code16
 
 start:
-    jmp entry
+    call main
 
-entry:
+/*
+ *  read_sector(dst_seg, dst_offset, drive, head, cylinder, sector, count)
+ *  dst_seg:es
+ *  dst_offset:bx
+ *  drive:dl
+ *  head:dh
+ *  cylinder:ch
+ *  sector:cl
+ *  count:al
+ */
+read_sector:
+    push %bp
+    mov %sp, %bp
+    // Put the stack data into specify registers
+    // The first argument is at 4(%bp), because call will put next IP in 2(%bp)
+    mov 0x4(%bp), %ax
+    mov %ax, %es
+    mov 0x6(%bp), %bx
+    mov 0x8(%bp), %dl
+    mov 0xa(%bp), %dh
+    mov 0xc(%bp), %ch
+    mov 0xe(%bp), %cl
+    mov 0x10(%bp), %al
+    // Read sector for int 0x13
+    mov $0x2, %ah
+    int $0x13
+    mov %bp, %sp
+    pop %bp
+    ret
+
+// puts(str)
+// str:di
+puts:
+    push %bp
+    mov %sp, %bp
+.next:
+    mov (%di), %al
+    add $1, %di
+    cmp $0, %al
+    je .fin
+    // Put character
+    mov $0x0e, %ah
+    int $0x10
+    jmp .next
+.fin:
+    leave
+    ret
+
+int_error:
+    mov $msg_int_err, %di
+    call puts
+
+    jmp end
+
+main:
+    // Init registers
     mov $0, %ax
     mov %ax, %ss
     mov %ax, %ds
     mov %ax, %es
+
+    // Init stack
     mov $0x7c00, %sp
+    mov %sp, %bp
 
-    // Start to read sector
+    // Enter
+    push %bp
+    mov %sp, %bp
 
-    // Dst addr es:bx
-    // The segment of dst addr(es)
-    mov $0x820, %ax
-    mov %ax, %es
-    // The offset of dst addr(bx)
-    mov $0, %bx
-
-    // Cylinder
-    mov $0, %ch
-    // Head
-    mov $0, %dh
-    // Sector
-    mov $1, %cl
-    // Read sector to mem
-    mov $0x2, %ah
-    // Sectors to read count
-    mov $1, %al
-    // Drive
-    mov $0, %dl
-    // Interrutp
-    int $0x13
-    // Error
-    jc puts_error
-
-    mov $msg_read_sector_done, %si
+    mov $msg_coolderOS, %di
     call puts
 
-puts:
-	mov (%si),%al
-	add $1,%si
-	cmp $0,%al
-	je fin
-	movb $0x0e,%ah
-	int $0x10
-	jmp puts
+    // read_sector(dst_seg, dst_offset, drive, head, cylinder, sector, count)
+    push $0x1
+    push $0x1
+    push $0x0
+    push $0x0
+    push $0x0
+    push $0x0
+    push $0x820
+    call read_sector
+    jc int_error
 
-msg_read_sector_done:
-    .asciz "Read sector done"
-
-puts_error:
-    mov $error_msg, %si
+    mov $msg_read_sector_done, %di
     call puts
 
-error_msg:
-    .asciz "Error occured"
+    // Leave
+    mov %bp, %sp
+    pop %bp
 
-fin:
+    jmp end
+
+end:
     hlt
-    jmp fin
+    jmp end
+
+msg_coolderOS:
+    .asciz "\r\n\n\rWelcome to coolderOS"
+msg_read_sector_done:
+    .asciz "\r\n\n\rRead sector done"
+msg_int_err:
+    .asciz "\r\n\n\rInterrupt error"
+msg_err:
+    .asciz "\r\n\n\rError occured"
 
 .org 510
 .word 0xaa55
