8800f60fad60eee5a60a990590b9fe9219d623ed
coolmsf <coolder.hsl@outlook.com>
2021-04-25 14:43:36 +0800

    backup

--- programming/go/test_project/Test/Test.go
+++ programming/go/test_project/Test/Test.go
@@ -1,25 +1,16 @@
 package main
 
-import (
-	"bytes"
-	"fmt"
-	"io"
-)
+// #include <stdio.h>
+// #include <stdlib.h>
+import "C"
+import "unsafe"
 
-type UpperWriter struct {
-	io.Writer
-}
-
-func (p *UpperWriter)Write(data []byte) (n int, err error) {
-	return p.Writer.Write(bytes.ToUpper(data))
-}
-
-func test(args ...interface{}) {
-	for _, i := range args{
-		fmt.Print(i)
-	}
+func PrintC(s string) {
+    cs := C.CString(s)
+    C.fputs(cs, (*C.FILE)(C.stdout))
+    C.free(unsafe.Pointer(cs))
 }
 
 func main() {
-	test(1, 2, 3, "Hello")
+    PrintC("Hello")
 }
